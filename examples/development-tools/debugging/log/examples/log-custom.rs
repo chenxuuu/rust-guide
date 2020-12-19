a63894c2fae00710777dac6920b4c333b223c828
zzy <9809920@qq.com>
2020-12-19 18:50:25 +0800

    日志工具，0.1

---
+++ examples/development-tools/debugging/log/examples/log-custom.rs
@@ -0,0 +1,32 @@
+use error_chain::error_chain;
+
+use log::LevelFilter;
+use log4rs::append::file::FileAppender;
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::config::{Appender, Config, Root};
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        LogConfig(log4rs::config::Errors);
+        SetLogger(log::SetLoggerError);
+    }
+}
+
+fn main() -> Result<()> {
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build("log/output.log")?;
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder()
+                   .appender("logfile")
+                   .build(LevelFilter::Info))?;
+
+    log4rs::init_config(config)?;
+
+    log::info!("Hello, world!");
+
+    Ok(())
+}
