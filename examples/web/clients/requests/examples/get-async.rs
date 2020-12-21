b9ff25729f87e016c2368112783b3b7071911ce4
zzy <9809920@qq.com>
2020-12-21 10:35:34 +0800

    请求处理，0.1

--- examples/web/clients/requests/examples/get-async.rs
+++ examples/web/clients/requests/examples/get-async.rs
@@ -1,5 +1,4 @@
 use error_chain::error_chain;
-use std::io::Read;
 
 error_chain! {
     foreign_links {
@@ -8,14 +7,13 @@ error_chain! {
     }
 }
 
-fn main() -> Result<()> {
-    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
-
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("http://httpbin.org/get").await?;
     println!("Status: {}", res.status());
     println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
 
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
     Ok(())
 }
